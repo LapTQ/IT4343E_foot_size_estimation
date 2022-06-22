3ef0a9d95df472faa2ddf6d440c6b4be897f04cf
ProgrammingForFun <lap.tq4@gmail.com>
2022-06-23 06:49:47 +0700

    Coding generate_data.py

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # foot_size_estimation
+
+
+# Generate training dataset
+
+```python
+python generate_data.py --train_num 1000 --val_num 100 
+```
--- generate_data.py
+++ generate_data.py
@@ -6,16 +6,23 @@ import os
 from pathlib import Path
 import random
 
+import albumentations as A
 
-ap = argparse.ArgumentParser()
 
-ap.add_argument('--train_num', type=int, default=1) # TODO 1000
-ap.add_argument('--val_num', type=int, default=1) # TODO 200
-ap.add_argument('--page', type=str, default=os.path.join('data', 'page'))
-ap.add_argument('--foot', type=str, default=os.path.join('data', 'foot'))
-ap.add_argument('--background', type=str, default=os.path.join('data', 'background'))
+def parse_opt():
 
-args = vars(ap.parse_args())
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument('--train_num', type=int, default=10) # TODO 1000
+    ap.add_argument('--val_num', type=int, default=1) # TODO 200
+    ap.add_argument('--page', type=str, default=os.path.join('data', 'page'))
+    ap.add_argument('--foot', type=str, default=os.path.join('data', 'foot'))
+    ap.add_argument('--background', type=str, default=os.path.join('data', 'background'))
+
+    args = vars(ap.parse_args())
+
+    return args
+
 
 IMG_EXTS = ['.jpg', '.jpeg', '.png']
 
@@ -47,32 +54,65 @@ def mask_page(pg_img):
 
 
 def mask_foot(ft_img):
-    # TODO cải tiến cho nhiều ảnh hơn
+    # TODO cải tiến tiếp
 
     hsv = cv2.cvtColor(ft_img, cv2.COLOR_BGR2HSV)
     hsv[:, :, 0] = cv2.medianBlur(hsv[:, :, 0], 3)
 
-    mask = np.where(np.logical_and(0 < hsv[:, :, 0], hsv[:, :, 0] < 17), 255, 0).astype('uint8')
+    mask = np.where(np.logical_and(0 < hsv[:, :, 0], hsv[:, :, 0] < 17), 255, 0).astype('uint8')    # TODO xem lại đoạn này, bỏ cái 0 < đi được không?
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=5)
 
     return mask
 
+def get_bg_transform(bg_paths, p=0.5):
+    return A.Compose([
+        A.RandomResizedCrop(height=512, width=512, scale=(0.01, 1.0), ratio=(0.2, 1.8), p=1),   # TODO square 512 is critical
+        A.RandomGridShuffle(p=p),
+        A.Flip(p=p),
+        A.Blur(p=p),
+        A.Perspective(p=p),
+        A.PiecewiseAffine(p=p),
+        A.SafeRotate(p=p),
+        A.Downscale(scale_min=0.5, scale_max=0.999, p=p),
+        A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=40, val_shift_limit=40, p=p),
+        A.FDA(bg_paths, p=p),
+        A.HistogramMatching(bg_paths, p=p),
 
-def synthesize(bg_img, pg_img, pg_mask, ft_img, ft_mask):
-    # TODO
-    # augment bg
-    # crop pg, crop ft
-    # resize pg, ft to adapt bg size
-    # geometric augment pg (same way with mask), ft (same way with mask)
-    # color/quality augment pg, ft
-    # past pg, ft to bg
+        # A.RandomBrightnessContrast(),
+        # A.ISONoise(),
+        # A.GridDropout(),
+        ])
 
-    syn_img = bg_img
-    syn_pg_mask = pg_mask
-    syn_ft_mask = ft_mask
 
-    return syn_img, syn_pg_mask, syn_ft_mask
+def get_pg_transform(p=0.5):
+    return A.Compose([
+        A.LongestMaxSize(max_size=512, p=1),
+        A.PadIfNeeded(512, 512, border_mode=cv2.BORDER_CONSTANT, p=1),
+        A.RandomResizedCrop(512, 512, scale=(2.0, 4.0), ratio=(1.0, 1.0), p=1),
+        A.SafeRotate(limit=180, border_mode=cv2.BORDER_CONSTANT, p=1),
+        A.Perspective(fit_output=True, p=p),
+        A.Downscale(scale_min=0.5, scale_max=0.999, p=p),
+        A.RandomBrightnessContrast(p=p),
+    ])
 
+
+def get_ft_transform(p=0.5):
+    return A.Compose([
+        A.LongestMaxSize(max_size=512, p=1),
+        A.PadIfNeeded(512, 512, border_mode=cv2.BORDER_CONSTANT, p=1),
+        A.RandomResizedCrop(512, 512, scale=(2.0, 4.0), ratio=(1.0, 1.0), p=1),
+        A.SafeRotate(limit=180, border_mode=cv2.BORDER_CONSTANT, p=1),
+        A.Perspective(fit_output=True, p=p),
+        A.Downscale(scale_min=0.5, scale_max=0.999, p=p),
+        A.RandomBrightnessContrast(p=p),
+        A.HueSaturationValue(p=p),
+    ])
+
+
+def paste(src, des, mask):
+    des = des.copy()
+    des[mask == 255] = src[mask == 255]
+    return des
 
 def main(args):
 
@@ -96,28 +136,61 @@ def main(args):
     # make dir for trainset and devset
     train_img_dir = make_dir('trainset', 'images')
     train_lbl_dir = make_dir('trainset', 'labels')
-    dev_img_dir = make_dir('devset', 'images')
-    dev_img_dir = make_dir('devset', 'labels')
 
     # TODO do for devset
     start = len(os.listdir(train_img_dir))
     for i in range(args['train_num']):
-        bg_img = random.choice(bg_images)
-        idx = random.randrange(len(pg_images))
-        pg_img, pg_msk = pg_images[idx], pg_masks[idx]
+
         idx = random.randrange(len(ft_images))
         ft_img, ft_msk = ft_images[idx], ft_masks[idx]
 
-        syn_img, syn_pg_msk, syn_ft_msk = synthesize(bg_img, pg_img, pg_msk, ft_img, ft_msk)
-        name = ('000000' + str(start + i))[-6:]
-        cv2.imwrite(os.path.join(train_img_dir, name + '.jpg'), syn_img)
-        cv2.imwrite(os.path.join(train_lbl_dir, name + '_1.jpg'), syn_pg_msk)
-        cv2.imwrite(os.path.join(train_lbl_dir, name + '_2.jpg'), syn_ft_msk)
+        # create new augmented background
+        bg_transform = get_bg_transform(bg_paths, 0.5)
+        bg_transformed = bg_transform(image=cv2.cvtColor(random.choice(bg_images), cv2.COLOR_BGR2RGB))
+        syn_bg = cv2.cvtColor(bg_transformed['image'], cv2.COLOR_RGB2BGR)
+
+        # create new augment page
+        idx = random.randrange(len(pg_images))
+        pg_img, pg_msk = pg_images[idx], pg_masks[idx]
+        pg_transform = get_pg_transform(0.5)
+        pg_transformed = pg_transform(image=cv2.cvtColor(pg_img, cv2.COLOR_BGR2RGB), mask=pg_msk)
+        syn_pg = cv2.cvtColor(pg_transformed['image'], cv2.COLOR_RGB2BGR)
+        syn_pg_mask = pg_transformed['mask']
+
+        # create new augment foot
+        idx = random.randrange(len(ft_images))
+        ft_img, ft_msk = ft_images[idx], ft_masks[idx]
+        ft_transform = get_ft_transform(0.5)
+        ft_transformed = ft_transform(image=cv2.cvtColor(ft_img, cv2.COLOR_BGR2RGB), mask=ft_msk)
+        syn_ft = cv2.cvtColor(ft_transformed['image'], cv2.COLOR_RGB2BGR)
+        syn_ft_mask = ft_transformed['mask']
+
+        syn_img = paste(syn_pg, syn_bg, syn_pg_mask)
+        syn_img = paste(syn_ft, syn_img, syn_ft_mask)
+
+        # create new augmented image
+        cv2.imwrite(f'trainset/labels/demo{i}.jpg', syn_img)
+        cv2.imwrite(f'trainset/labels/demo{i}_1.jpg', syn_pg_mask)
+        cv2.imwrite(f'trainset/labels/demo{i}_2.jpg', syn_ft_mask)
+
+
+
+
+
+
+
+
+        # name = ('000000' + str(start + i))[-6:]
+        # cv2.imwrite(os.path.join(train_img_dir, name + '.jpg'), syn_img)
+        # cv2.imwrite(os.path.join(train_lbl_dir, name + '_1.jpg'), syn_pg_msk)
+        # cv2.imwrite(os.path.join(train_lbl_dir, name + '_2.jpg'), syn_ft_msk)
 
 
 if __name__ == '__main__':
 
-    # main(args)
+    args = parse_opt()
 
-    for i in range(1, 10):
-        cv2.imwrite(f'trainset/labels/demo{i}.jpg', mask_foot(cv2.imread(f'data/foot/{i}.jpg')))
+    main(args)
+
+    # for i in range(10):
+    #     cv2.imwrite(f'trainset/labels/demo{i}.jpg', synth([cv2.imread(str(path)) for path in Path('data/background').glob('*')], None, None, None, None)[0])
