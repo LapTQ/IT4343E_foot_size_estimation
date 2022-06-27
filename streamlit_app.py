2f8777d5409b1c6c71314a6caeb94f0f801d5076
ProgrammingForFun <lap.tq4@gmail.com>
2022-06-28 00:47:53 +0700

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from PIL import Image
 import cv2
-
+import numpy as np
 
 st.title('Foot size estimation')
 
 file = st.file_uploader('Upload image')
 
 if file:
-    img = Image.open(file)
+    img = np.array(Image.open(file))
     cv2.imwrite('demo.jpg', img)
     img = cv2.imread('demo.jpg')
     st.image(Image.fromarray(img))
