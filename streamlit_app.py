da20a4eb2b8b6d33a42cd1a3ef64ce4dda6fd592
ProgrammingForFun <lap.tq4@gmail.com>
2022-06-28 02:59:41 +0700

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,29 @@ from PIL import Image
 import cv2
 import numpy as np
 
+import torch
+from torchvision.transforms import ToTensor
+
+from models.unet import UNet
+
 st.title('Foot size estimation')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+net = UNet(3, 2).to(device)
+net.load_state_dict(torch.load('weights/ckpt.pth', map_location=device))
 
 file = st.file_uploader('Upload image')
 
 if file:
     img = np.array(Image.open(file))
-    cv2.imwrite('demo.jpg', img)
-    img = cv2.imread('demo.jpg')
-    st.image(Image.fromarray(img))
+    scale = 350 / max(img.shape)
+
+    W, H = int(img.shape[1] * scale), int(img.shape[0] * scale)
+    img = cv2.resize(img, (W, H))
+    x = ToTensor()(img[:, :, ::-1].copy()).unsqueeze(0)
+
+    y = np.where(net(x).squeeze() > 0.5, 1, 0).astype('uint8')
+    pg_mask, ft_mask = y[0], y[1]
+
+    collage = np.concatenate([pg_mask, ft_mask], axis=1)
+    st.image(Image.fromarray(collage))
