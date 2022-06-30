840d7c3925e4db27cef3ba0aafcced3df93ef192
ProgrammingForFun <lap.tq4@gmail.com>
2022-06-30 08:13:09 +0700

    msg

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,87 +32,89 @@ def arrange_corners(corners):
     corners = sorted(corners[:2], key=lambda x: x[1]) + sorted(corners[2:], key=lambda x: x[1], reverse=True)
     return np.array(corners).reshape(shape)
 
+def main():
+    if file:
+        img = Image.open(file)
+        st.image(img)
 
-if file:
-    img = Image.open(file)
-    st.image(img)
+        img = np.array(img.convert('RGB'))
+        scale = 350 / max(img.shape)
+        W, H = int(img.shape[1] * scale), int(img.shape[0] * scale)
+        img = cv2.resize(img, (W, H))
 
-    img = np.array(img.convert('RGB'))
-    scale = 350 / max(img.shape)
-    W, H = int(img.shape[1] * scale), int(img.shape[0] * scale)
-    img = cv2.resize(img, (W, H))
+        x = ToTensor()(img.copy()).unsqueeze(0)
 
-    x = ToTensor()(img.copy()).unsqueeze(0)
+        net.eval()
+        with torch.no_grad():
+            y = torch.sigmoid(net(x)['out'])
 
-    net.eval()
-    with torch.no_grad():
-        y = torch.sigmoid(net(x)['out'])
+        mask = (y.cpu().squeeze().numpy() * 255).astype('uint8')
 
-    mask = (y.cpu().squeeze().numpy() * 255).astype('uint8')
+        plt.figure(figsize=(20, 20))
+        plt.subplot(2, 2, 1);
+        plt.imshow(mask)
 
-    plt.figure(figsize=(20, 20))
-    plt.subplot(2, 2, 1);
-    plt.imshow(mask)
+        choices = []
+        for thresh in range(254, 0, -1):
+            _, threshed = cv2.threshold(mask, thresh, 255, cv2.THRESH_BINARY)
+            retval, labels, stats, centroids = cv2.connectedComponentsWithStats(threshed, connectivity=4)
+            if 2 <= retval <= 4:  # including background
+                choices.append(thresh)
+        thresh = int(np.mean(choices))
+        _, mask = cv2.threshold(mask, thresh, 255, cv2.THRESH_BINARY)
 
-    choices = []
-    for thresh in range(254, 0, -1):
-        _, threshed = cv2.threshold(mask, thresh, 255, cv2.THRESH_BINARY)
-        retval, labels, stats, centroids = cv2.connectedComponentsWithStats(threshed, connectivity=4)
-        if 2 <= retval <= 4:  # including background
-            choices.append(thresh)
-    thresh = int(np.mean(choices))
-    _, mask = cv2.threshold(mask, thresh, 255, cv2.THRESH_BINARY)
+        plt.subplot(2, 2, 2);
+        plt.imshow(mask)
 
-    plt.subplot(2, 2, 2);
-    plt.imshow(mask)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        hull = cv2.convexHull(np.concatenate(contours, axis=0))
 
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    hull = cv2.convexHull(np.concatenate(contours, axis=0))
+        fail = True
+        for alpha in np.arange(0.01, 0.5, 0.01):
+            epsilon = alpha * cv2.arcLength(hull, True)
+            approx = cv2.approxPolyDP(hull, epsilon, True)
+            if approx.shape[0] == 4:
+                fail = False
+                break
 
-    fail = True
-    for alpha in np.arange(0.01, 0.5, 0.01):
-        epsilon = alpha * cv2.arcLength(hull, True)
-        approx = cv2.approxPolyDP(hull, epsilon, True)
-        if approx.shape[0] == 4:
-            fail = False
-            break
+        if fail:
+            st.write('Cannot estimate with this image. Please capture again.')
+            return
 
-    if fail:
-        st.write('Cannot estimate with this image. Please capture again.')
-        return
+        demo = cv2.drawContours(cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), [approx], -1, (0, 255, 0))
+        plt.subplot(2, 2, 3);
+        plt.imshow(demo)
 
-    demo = cv2.drawContours(cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), [approx], -1, (0, 255, 0))
-    plt.subplot(2, 2, 3);
-    plt.imshow(demo)
+        approx = arrange_corners(approx)
+        l = [np.sqrt(np.sum((approx[i] - approx[(i + 1) % 4]) ** 2)) for i in range(4)]
+        if np.argmax(l) % 2 == 1:
+            w, h = 297, 210
+        else:
+            w, h = 210, 297
+        dst = np.array([
+            [0, 0],
+            [0, h],
+            [w, h],
+            [w, 0]
+        ])
+        M = cv2.getPerspectiveTransform(approx.astype('float32'), dst.astype('float32'))
+        mask = cv2.warpPerspective(mask, M, (w, h), flags=cv2.INTER_NEAREST)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=15)
 
-    approx = arrange_corners(approx)
-    l = [np.sqrt(np.sum((approx[i] - approx[(i + 1) % 4]) ** 2)) for i in range(4)]
-    if np.argmax(l) % 2 == 1:
-        w, h = 297, 210
-    else:
-        w, h = 210, 297
-    dst = np.array([
-        [0, 0],
-        [0, h],
-        [w, h],
-        [w, 0]
-    ])
-    M = cv2.getPerspectiveTransform(approx.astype('float32'), dst.astype('float32'))
-    mask = cv2.warpPerspective(mask, M, (w, h), flags=cv2.INTER_NEAREST)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=15)
+        mask = 255 - mask
+        retval, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=4)
+        k = sorted([(k, stats[k, cv2.CC_STAT_AREA]) for k in range(1, retval)], key=lambda x: x[1], reverse=True)[0][0]
+        (x, y), (w, h), alpha = cv2.minAreaRect(np.roll(np.where(labels == k), 1, axis=0).transpose().reshape(-1, 1, 2))
 
-    mask = 255 - mask
-    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=4)
-    k = sorted([(k, stats[k, cv2.CC_STAT_AREA]) for k in range(1, retval)], key=lambda x: x[1], reverse=True)[0][0]
-    (x, y), (w, h), alpha = cv2.minAreaRect(np.roll(np.where(labels == k), 1, axis=0).transpose().reshape(-1, 1, 2))
+        demo = cv2.drawContours(cv2.cvtColor(255 - mask, cv2.COLOR_GRAY2BGR),
+                                [np.int0(cv2.boxPoints(((x, y), (w, h), alpha)))], -1, (0, 255, 0))
+        plt.subplot(2, 2, 4);
+        plt.imshow(demo)
 
-    demo = cv2.drawContours(cv2.cvtColor(255 - mask, cv2.COLOR_GRAY2BGR),
-                            [np.int0(cv2.boxPoints(((x, y), (w, h), alpha)))], -1, (0, 255, 0))
-    plt.subplot(2, 2, 4);
-    plt.imshow(demo)
+        plt.tight_layout()
+        plt.savefig('demo.png')
 
-    plt.tight_layout()
-    plt.savefig('demo.png')
+        st.image(Image.open('demo.png'))
+        st.write(f'Estimated size: {(297 - 2 * (297 - max(w, h))) / 10}')
 
-    st.image(Image.open('demo.png'))
-    st.write(f'Estimated size: {(297 - 2 * (297 - max(w, h))) / 10}')
+main()
